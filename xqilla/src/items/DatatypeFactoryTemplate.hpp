58d729dba7f2d6c9f301ff6b15b9b2c0c65d7cb3
Daniël de Kok <me@danieldk.eu>
2014-12-02 10:54:29 +0100

    Make DB XML buildable with gcc >= 4.7.
    The method to-be called could not be resolved.

--- xqilla/src/items/DatatypeFactoryTemplate.hpp
+++ xqilla/src/items/DatatypeFactoryTemplate.hpp
@@ -79,7 +79,7 @@ public:
   AnyAtomicType::Ptr createInstance(const XMLCh* value,
                                     const DynamicContext* context) const
   {
-    return createInstanceNoCheck(DatatypeFactoryTemplate<TYPE>::getPrimitiveTypeURI(),
+    return this->createInstanceNoCheck(DatatypeFactoryTemplate<TYPE>::getPrimitiveTypeURI(),
                                  DatatypeFactoryTemplate<TYPE>::getPrimitiveTypeName(), value, context);
   }
 
